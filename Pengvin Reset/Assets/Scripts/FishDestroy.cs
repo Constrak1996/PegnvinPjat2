9aff92c1fedfd155a8d2413a8cca763a8b994c60
Alex Frier Stewart <alex7175@student.eadania.dk>
2019-04-03 20:31:14 +0200

    Fish who reach to bottom are destroyed

---
+++ Pengvin Reset/Assets/Scripts/FishDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDestroy : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScrapFish")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
