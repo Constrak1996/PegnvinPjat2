856d03fdcd45729cc86b88d4f59b4425536ed2d6
Alex Frier Stewart <alex7175@student.eadania.dk>
2019-04-04 12:10:13 +0200

    update

---
+++ Pengvin Reset/Assets/Scripts/Bomb/ExplosionSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSpawn : MonoBehaviour
+{
+    public Transform explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            explosion.GetComponent<ParticleSystem>().Play();
+        }        
+    }
+
+    IEnumerable stopExplosion()
+    {
+        yield return new WaitForSeconds(0.4f);
+    }
+}
